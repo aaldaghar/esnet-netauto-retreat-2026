41c982a04bbf4a0bc9198f6683e293ddc076997e
Oliver <oliver@es.net>
2026-01-23 11:29:11 -0500

    adding oliver's completed range_only and split files

--- team_files/oliver/range_only.py
+++ team_files/oliver/range_only.py
@@ -1,11 +1,9 @@
 import ipaddress
 import os
 import json
-import pprint
 
 FILES = "/Users/oliver/src/esnet-netauto-retreat-2026/exercise_files"
-v4_list = []
-v6_list =  []
+prefix_list = []
 
 config_path = os.path.join(FILES, "ipls.json")
 
@@ -16,18 +14,17 @@ for entry in prefix_dict.values():
         for v in entry.values():
             for dic in v:
                 prefix = dic.get('ip_prefixes')
-                ip = prefix.split('/')
+                prefix_type = dic.get('type')
+
                 try:
-                    if ipaddress.ip_address(ip[0]).version == 4:
-                        v4_list.append(prefix)
-                    elif ipaddress.ip_address(ip[0]).version == 6:
-                        v6_list.append(prefix)
+                    if prefix_type == 'range':
+                        prefix_list.append(prefix)
                 except ValueError as err:
                     print ('*' *55)
                     print (f"INFO: '{prefix}' is not a valid prefix value.")
                     print ('*' *55)
 
+print() 
+print (f"A total of {len(prefix_list)} prefixes with type = 'range' have been found: \n\n Prefixes:{prefix_list}")
 print()
-print ("List of V4 prefixes: ",v4_list)
-print("List V6 prefixes: ",v6_list)
-print()
+
